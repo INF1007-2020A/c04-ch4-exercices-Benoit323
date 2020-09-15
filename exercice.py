ca2bd8f0149a8162b6f36d93e74f5a61ed931495
Benoit323 <benoit.dambrine@polymtl.ca>
2020-09-15 23:18:11 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -20,6 +20,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
     if old_char in string:
         old_char = new_char
         
+        
 
 
 def get_number_of_char(string: str, char: str) -> int:
