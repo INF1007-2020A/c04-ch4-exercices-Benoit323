8ae4cf72230cb7be30d4bed4fff17eec31f7a862
Benoit323 <benoit.dambrine@polymtl.ca>
2020-09-15 22:46:28 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -10,8 +10,7 @@ def is_even_len(string: str) -> bool:
 def remove_third_char(string: str) -> str:
     for i in range(len(string)):
         if i == 3:
-            string[x] = 
-            
+            string[x] = ""      
 
     
 
