129f0d9cc22ac59282161145862b6e96a4089b7b
Benoit323 <benoit.dambrine@polymtl.ca>
2020-09-15 23:23:39 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -31,6 +31,7 @@ def get_number_of_char(string: str, char: str) -> int:
 
 def get_number_of_words(sentence: str, word: str) -> int:
     for word in sentence:
+        if 
         a =+ 1
     return a
 
