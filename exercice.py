f921b655ef39a71c27a5e8c9a3a166ed9f2addc7
Benoit323 <benoit.dambrine@polymtl.ca>
2020-09-15 23:36:28 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,8 @@ def remove_third_char(string: str) -> str:
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    for old_char in string
+    for old_char in string:
+        
 
         
 
